fe049470e03177e8c96e27f5d7c34a20f0569779
nepsoul <nirumagar901@gmail.com>
2023-08-03 08:51:01 +0545

    chore: completed exercise 3.7-3.7

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const App = express();
 App.use(express.json());
+App.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
@@ -26,14 +41,28 @@ let persons = [
   },
 ];
 
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+  ].join(" ");
+});
+
 App.get("/", (request, response) => {
   response.send("<h1>hello<h1/>");
 });
 
 App.get("/info", (request, response) => {
-    const personLength=persons.length
-    response.send(`Phonebook has info for ${personLength} people <br/> ${new Date()}`);
-  });
+  const personLength = persons.length;
+  response.send(
+    `Phonebook has info for ${personLength} people <br/> ${new Date()}`
+  );
+});
 
 App.get("/persons", (request, response) => {
   response.json(persons);
@@ -48,7 +77,6 @@ App.get("/persons/:id", (request, response) => {
       .json({ error: 404, message: `there is no person with id` + currentId });
   // .end();
   console.log(thisPerson);
-
 });
 App.delete("/persons/:id", (request, response) => {
   const currentId = Number(request.params.id);
@@ -57,9 +85,24 @@ App.delete("/persons/:id", (request, response) => {
 });
 App.post("/persons", (request, response) => {
   let newData = request.body;
-  
-  newData.id = Math.floor(Math.random()*persons.length+1000000000000000)
 
+  newData.id = Math.floor(Math.random() * persons.length + 1000000000000000);
+  let existedData = persons.find((person) => {
+    return person.name === newData.name;
+  });
+  console.log(newData, "newdata");
+
+  if (existedData) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
+  if (
+    newData.name === "" ||
+    newData.number === "" ||
+    !newData.hasOwnProperty("name") ||
+    !newData.hasOwnProperty("number")
+  ) {
+    return response.status(400).json({ error: "name or number is missing" });
+  }
   persons.push(newData);
 
   response.status(201).json(newData);
